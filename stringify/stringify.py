98755609f15288fd7fc6c2a520a81e8e8e854de8
Jason R. Jones <slightlynybbled@gmail.com>
2017-10-23 13:54:16 -0400

    Refactored for easier readability

--- stringify/stringify.py
+++ stringify/stringify.py
@@ -21,28 +21,29 @@ def stringify(binary_file_path):
     return encoded_string
 
 
-def _file_name_to_var(binary_file_path):
+def _file_name_to_var(source_file_path):
     """
     converts a file path into a python-valid variable name
-    :param binary_file_path: the full path, including extension
+    :param source_file_path: the full path, including extension
     :return: a valid python variable name
     """
-    var_name = os.path.basename(binary_file_path).split('.')[0]  # remove the extension
+    var_name = os.path.basename(source_file_path).split('.')[0]  # remove the extension
     var_name = var_name.replace('-', '_')  # replace dashes with underscores
+    var_name = var_name.replace(' ', '_')  # replace dashes with underscores
 
     return var_name
 
 
-def bin_to_py_file(binary_file_path, destination_file='binary.py', overwrite=True):
+def bin_to_py_file(source_file_path, destination_file='binary.py', overwrite=True):
     """
     creates a single file containing a single python variable
 
-    :param binary_file_path: path to a binary file
+    :param source_file_path: path to a binary file
     :param destination_file: the path to which to save the new variable, including the '.py' extension
     :param overwrite: True or False
     :return: None
     """
-    logger.debug('attempting to save {} to {}'.format(binary_file_path, destination_file))
+    logger.debug('attempting to save {} to {}'.format(source_file_path, destination_file))
 
     if os.path.exists(destination_file) and not overwrite:
         raise ValueError('the file {} already exists and the overwrite flag is False'.format(destination_file))
@@ -50,11 +51,11 @@ def bin_to_py_file(binary_file_path, destination_file='binary.py', overwrite=Tru
     if os.path.splitext(destination_file)[1] != '.py':
         raise ValueError('the destination file must be a python type (extension must be ".py")')
 
-    if not os.path.isfile(binary_file_path):
+    if not os.path.isfile(source_file_path):
         raise ValueError('the file specified is not a file or does not exist')
 
-    var_name = _file_name_to_var(binary_file_path)
-    var_data = stringify(binary_file_path)
+    var_name = _file_name_to_var(source_file_path)
+    var_data = stringify(source_file_path)
 
     py_file = '{} = {}\n'.format(var_name, var_data)
 
@@ -62,17 +63,17 @@ def bin_to_py_file(binary_file_path, destination_file='binary.py', overwrite=Tru
         f.write(py_file)
 
 
-def dir_to_py_file(directory_path, destination_file='binary.py', overwrite=True):
+def dir_to_py_file(source_path, destination_file='binary.py', overwrite=True):
     """
     creates a single python file containing multiple variables representing binary files found
     in the 'directory_path'
 
-    :param directory_path: path to a directory
+    :param source_path: path to a directory
     :param destination_file: the path to which to save the new variables, including the '.py' extension
     :param overwrite: True or False
     :return: None
     """
-    logger.debug('attempting to save all files from {} to {}'.format(directory_path, destination_file))
+    logger.debug('attempting to save all files from {} to {}'.format(source_path, destination_file))
 
     if os.path.exists(destination_file) and not overwrite:
         raise ValueError('the file {} already exists and the overwrite flag is False'.format(destination_file))
@@ -80,12 +81,12 @@ def dir_to_py_file(directory_path, destination_file='binary.py', overwrite=True)
     if os.path.splitext(destination_file)[1] != '.py':
         raise ValueError('the destination file must be a python type (extension must be ".py")')
 
-    if not os.path.isdir(directory_path):
+    if not os.path.isdir(source_path):
         raise ValueError('the path specified is not a directory')
 
     py_file = ''
 
-    for root, _, files in os.walk(directory_path):
+    for root, _, files in os.walk(source_path):
         for name in files:
             file_path = os.path.join(root, name)
 
